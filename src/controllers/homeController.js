56a5f991f070b01359e67db25a25003fae3a747a
ThanhThat <thachthanhthat1995@gmail.com>
2024-09-09 23:54:54 +0700

    Create navbar in hompage.

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,13 +3,10 @@ const connection = require("../config/database");
 const getHomepage = (req, res) => {
   let users = [];
   // Get data in database <=> simple query
-
   connection.query("SELECT * FROM users", function (err, result, fields) {
     users = result;
-    console.log("check result >> ", result);
-
-    console.log("check user >> ", users);
-    res.send(JSON.stringify(users));
+    // res.send(JSON.stringify(users));
+    return res.render("home.ejs");
   });
 };
 
